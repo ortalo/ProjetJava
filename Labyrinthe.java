e3f4bbd2123a76137296905fbbcd5a8276487bb4
ortalo <pierre.ortalo@gmail.com>
2017-02-06 15:26:40 +0100

    Update Labyrinthe.java

--- Labyrinthe.java
+++ Labyrinthe.java
@@ -7,17 +7,17 @@ public class Labyrinthe implements Test{
 	public double apprentissage(int semaine){
 		int lundi=semaine*5; // Pour semaine 0: lundi =0 vendredi =4, semaine 1: lundi=5, vendredi=9, semaine 2: 10,14...
 		int vendredi=lundi+4;
-		return (this.temps[lundi]-this.temps[vendredi])*100/this.temps[lundi];
+		return (this.temps.elementAt(lundi)-this.temps.elementAt(vendredi))*100/this.temps.elementAt(lundi);
 	}
 	public void setResultats(){
 		System.out.println("Saisir le temps pour sortir du labyrinthe (secs): ");
 		int temps=Etude.saisie_entier;
-		this.temps[Etude.getJour()]=temps;
+		this.temps.elementAt(Etude.getJour())=temps;
 	}
 	public void getBestDay(int semaine){
 		int lundi=semaine*5; // Pour semaine 0: lundi =0 vendredi =4, semaine 1: lundi=5, vendredi=9, semaine 2: 10,14...
 		int vendredi=lundi+4;
-		int min=this.temps[lundi];
+		int min=this.temps.elementAt(lundi);
 		int day= new int();
 		for(int i=lundi;i<=vendredi;i++){
 			int current=this.temps.elementAt(i);
